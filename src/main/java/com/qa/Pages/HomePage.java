05e56775006d664b06bce456ceaefac1a3186992
kk mishra <kkmjssate@gmail.com>
2020-07-31 20:43:26 +0530

    Update pom and add Home page

---
+++ src/main/java/com/qa/Pages/HomePage.java
@@ -0,0 +1,11 @@
+package com.qa.Pages;
+
+public class HomePage {
+	
+	public void homePage() {
+		System.out.println("home page");
+	}
+	public void homePageSet() {
+		System.out.println("home page Set");
+	}
+}
